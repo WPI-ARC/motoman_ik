9f61333a43be294cc1a362d86dc4e25f3870efa1
panyixiao123 <panyixiao@126.com>
2015-03-09 18:32:12 -0400

    auto model loader added

--- auto_test/script/IK_solver_test.py
+++ auto_test/script/IK_solver_test.py
@@ -21,8 +21,6 @@ from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from sensor_msgs.msg import JointState
 from goal_pos_generate import generate_goal_points
 
-#global trajectory
-#trajectory = []
 
 def Get_current_state(group):
 	return	JointState(
@@ -88,6 +86,7 @@ def Copy_joint_value(group_name, joint_values):
 		count += 1;
 					
 	return Target_joint_value;
+	
 		
 def pos_test(pose_targets, group_handle, IK_handle, animate_result = False):
   
@@ -99,7 +98,8 @@ def pos_test(pose_targets, group_handle, IK_handle, animate_result = False):
 	 ik = IK_handle;
   else:
 	  print "No IK solver assigned! Exit!";
-	  return False;  
+	  return False;
+	  
   if len(pose_targets):
 	count = 0;
 	success_number = 0;
@@ -110,51 +110,40 @@ def pos_test(pose_targets, group_handle, IK_handle, animate_result = False):
 		target.orientation.x = 0.50;
 		target.orientation.y = -0.50;		
 		target.orientation.z = -0.50;
-		target.orientation.w = 0.507;		
+		target.orientation.w = 0.507;	
 		target.position.x = pose_target.x;
 		target.position.y = pose_target.y;
-		target.position.z = pose_target.z;	
+		target.position.z = pose_target.z;
+		
 		print target.position;	
 		current_state = Get_current_state(group);
 		result = find_IK_solution(ik, target, current_state, group_handle.get_name());		
 		
-		#print result.solution.joint_state;
-		
 		if result.error_code.val != 1:
-			#print "Can't find solution for No.",count,"target";
 			print "Bin ", Bin_number(count), "test failed!";
-		else:
-			#print result.solution.joint_state;
-			success_number += 1;
+			
+		else:			
+			#print result.solution.joint_state;			
+			success_number += 1;			
 			TargetJointValue = Copy_joint_value(group_handle.get_name(),result.solution.joint_state.position);
-			#print "Target joint value:", TargetJointValue;			
+			
+			#print "Target joint value:", TargetJointValue;				
+			
 			if animate_result:
 				print ">>>>>>>>>>>>>>> Displaying No.", count, "trajectory >>>>>>>>>>>>>>>"
 				if len(TargetJointValue):
-<<<<<<< HEAD:auto_test/script/IK_solver_test.py
-					   group.set_joint_value_target(TargetJointValue);
-					   plan = group.plan();	
-					   #rospy.sleep(2);
-					   #print ">>>>>>>>>>>>> Actuate trajectory >>>>>>>>>>>>>"
-					   group.go();
-					   rospy.sleep(2);
-=======
 					group.set_joint_value_target(TargetJointValue);
 					plan = group.plan();
-					if( count%2 == 0):
-					  buf = StringIO();
-					  plan.serialize(buf);
-					  print "traj",count," in text"
-					  #trajectory.append(buf.getvalue())
-					  file_name = "traj_file"+str(count)
-					  f = open(file_name,"w") 
-					  f.write(buf.getvalue())
-					  f.close()					  	
-					#rospy.sleep(2);
-					#print ">>>>>>>>>>>>> Actuate trajectory >>>>>>>>>>>>>"
+					rospy.sleep(2);
+					if(count%2 == 0):
+						buf = StringIO();
+						plan.serialize(buf);
+						print "traj",count," in text";
+						file_name = "traj_file"+str(count);
+						f = open(file_name,"w") ;
+						f.write(buf.getvalue());
+						f.close();							  	
 					group.go();
-					#rospy.sleep(2);
->>>>>>> 8f0348680a81ade1e71be611aab576081e657106:auto_test/IK_solver_test.py
 					
 	if success_number == test_number:
 		print "Available for all position!";
@@ -166,13 +155,13 @@ def pos_test(pose_targets, group_handle, IK_handle, animate_result = False):
   else:
 	  print "No target Assigned, Exit!";
 	  return False;
-	
 
 if __name__=='__main__':
   try:
+	  
 	#Init_Right_Hand_Pos = [0.323265,-0.499221,1.07222, -0.4838, 0.515602, 0.5159, -0.48353];
+	
 	if len(sys.argv)>1:
-	  #print len(sys.argv);
 	  X_pos = float(sys.argv[1]);
 	  Y_pos = float(sys.argv[2]);
 	  Z_pos = float(sys.argv[3]);
@@ -181,7 +170,7 @@ if __name__=='__main__':
 	  X_pos = 1.32;
 	  Y_pos = 0;
 	  Z_pos = 0;
-	
+	  
 	Goal_points = generate_goal_points(Bin_base_x = X_pos, Bin_base_y = Y_pos, Bin_base_z = Z_pos, Test_Depth = 0.2);
 	
 	print "============ Initializing... ============="
@@ -189,15 +178,10 @@ if __name__=='__main__':
 	rospy.init_node('IK_Solution_Test', anonymous=True);  
 	robot = moveit_commander.RobotCommander();
 	scene = moveit_commander.PlanningSceneInterface();
-<<<<<<< HEAD:auto_test/script/IK_solver_test.py
+	
 	arm_left_group = moveit_commander.MoveGroupCommander("arm_left");	
 	arm_left_group.set_planner_id("RRTstarkConfigDefault");	
-=======
-	arm_left_group = moveit_commander.MoveGroupCommander("arm_left");
-	
-	arm_left_group.set_planner_id("RRTstarkConfigDefault");
 
->>>>>>> 8f0348680a81ade1e71be611aab576081e657106:auto_test/IK_solver_test.py
 	print ">>>>>>>>>>>>> Waiting for service `compute_ik` >>>>>>>>>>>>";
 	rospy.wait_for_service('compute_ik');
 	ik = rospy.ServiceProxy("compute_ik", GetPositionIK);
