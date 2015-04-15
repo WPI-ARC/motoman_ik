617a22abd39f129843e2adbda2318eda1166b03e
Alex Henning <alex@thoriumrobotics.com>
2015-04-15 13:02:55 -0400

    Fixes for publishing collision objects

--- apc_models/src/publish_mesh.py
+++ apc_models/src/publish_mesh.py
@@ -11,24 +11,36 @@ from geometry_msgs.msg import PoseStamped
 
 
 if __name__=='__main__':
+    scene = moveit_commander.PlanningSceneInterface()
+    rospy.init_node('publish_mesh', anonymous=True)
     rospy.sleep(10)
-    rospy.init_node('publish_mesh', anonymous=True);
-    scene = moveit_commander.PlanningSceneInterface();
 
     name, stl = sys.argv[1], sys.argv[2]
-    pose = PoseStamped();
-    pose.pose.position.x = float(sys.argv[3]);
-    pose.pose.position.y = float(sys.argv[4]);
-    pose.pose.position.z = float(sys.argv[5]);
-    pose.pose.orientation.x = float(sys.argv[6]);	
-    pose.pose.orientation.y = float(sys.argv[7]);	
-    pose.pose.orientation.z = float(sys.argv[8]);	
-    pose.pose.orientation.w = float(sys.argv[9]);
-        
-    scene.attach_mesh(link = "base_link", 
-                      name = name, 
-                      pose = pose,
-                      filename = stl);
-        
+    pose = PoseStamped()
+    pose.header.frame_id = "/base_link"
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position.x = float(sys.argv[3])
+    pose.pose.position.y = float(sys.argv[4])
+    pose.pose.position.z = float(sys.argv[5])
+    pose.pose.orientation.x = float(sys.argv[6])
+    pose.pose.orientation.y = float(sys.argv[7])
+    pose.pose.orientation.z = float(sys.argv[8])
+    pose.pose.orientation.w = float(sys.argv[9])
+
+    # mesh = scene._PlanningSceneInterface__make_mesh(
+    #     name="shelf",
+    #     pose=pose,
+    #     filename=stl
+    # )
+    # mesh.header.frame_id = "/base_link"
+    # mesh.header.stamp = rospy.Time.now()
+    # scene._pub_co.publish(mesh)
+
+    scene.add_mesh(
+        name=name,
+        pose=pose,
+        filename=stl
+    )
+
     print "Published "+name
     moveit_commander.roscpp_shutdown()
